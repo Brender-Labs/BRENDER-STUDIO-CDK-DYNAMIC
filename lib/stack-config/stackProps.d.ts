163cb0bba19f8daf5371d2c901f30f45c1d76f39
david-blg <dav.stack.dev@gmail.com>
2024-02-13 13:34:44 +0100

    refactor: add names for each resource

--- lib/stack-config/stackProps.d.ts
+++ lib/stack-config/stackProps.d.ts
@@ -3,6 +3,5 @@ import * as cdk from 'aws-cdk-lib';
 
 export interface BrenderStudioStackProps extends cdk.StackProps {
   blenderVersionsList: string;
-  brenderBucketName: string;
   isPrivate: boolean;
 }
